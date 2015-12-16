162ed706c7f4be0356871a4a6f8bb6398b38854f
Bernhard <bernhard.gruenewaldt@comsysto.com>
2015-12-16 14:22:42 +0100

    step 01 - after atlas-create-jira-plugin-module Webwork Module

--- step-01-kitchen-duty-plugin/README.md
+++ step-01-kitchen-duty-plugin/README.md
@@ -1,6 +1,6 @@
 # Kitchen Duty Plugin for Atlassian JIRA®
 
-## Tutorial Step 0
+## Tutorial Step 1
 
 [![](https://comsysto.github.io/kitchen-duty-plugin-for-atlassian-jira/images/kitchen-duty-teaser.png)](https://comsysto.github.io/kitchen-duty-plugin-for-atlassian-jira/)
 
---
+++ step-01-kitchen-duty-plugin/src/main/java/com/comsysto/kitchen/duty/webwork/KitchenDutyPlanningWebworkAction.java
@@ -0,0 +1,16 @@
+package com.comsysto.kitchen.duty.webwork;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.atlassian.jira.web.action.JiraWebActionSupport;
+
+public class KitchenDutyPlanningWebworkAction extends JiraWebActionSupport
+{
+    private static final Logger log = LoggerFactory.getLogger(KitchenDutyPlanningWebworkAction.class);
+
+    @Override
+    public String execute() throws Exception {
+
+        return super.execute(); //returns SUCCESS
+    }
+}

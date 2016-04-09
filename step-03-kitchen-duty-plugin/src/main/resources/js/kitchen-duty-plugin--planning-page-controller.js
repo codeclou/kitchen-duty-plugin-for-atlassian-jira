7a3856ce2c7abdbf389989ea2f02225a814316d0
Bernhard <bernhard.gruenewaldt@comsysto.com>
2016-04-09 12:04:23 +0200

    remove handlebars.js in favor of soy. make aui select2 user search work

--- step-03-kitchen-duty-plugin/src/main/resources/js/kitchen-duty-plugin--planning-page-controller.js
+++ step-03-kitchen-duty-plugin/src/main/resources/js/kitchen-duty-plugin--planning-page-controller.js
@@ -1,9 +1,56 @@
-AJS.toInit(function(){
+var showSuccessFlag = function(message) {
     require(['aui/flag'], function(flag) {
         var myFlag = flag({
             type: 'success',
             title: 'Kitchen Duty Plugin',
-            body: 'JS Controller is working'
+            close: 'auto',
+            body: message
         });
     });
+};
+
+var initUserSearch = function(restUrl) {
+    var templateUserSearch = JIRA.Templates.KDP.userSearch();
+
+    var auiUserSelectOptions = {
+        ajax: {
+            url: function () {
+                return restUrl + '/user/search';
+            },
+            dataType: 'json',
+            delay: 250,
+            data: function (searchTerm) {
+                return {
+                    query: searchTerm
+                };
+            },
+            results: function (data) {
+                return {
+                    results: data
+                };
+            },
+            cache: true
+        },
+        minimumInputLength: 1,
+        tags: 'true'
+    };
+
+    /* INIT TEMPLATES AND WIDGETS */
+
+    AJS.$('#kdp-planning-page-container').append(templateUserSearch);
+    AJS.$('#kdp-user-select').auiSelect2(auiUserSelectOptions);
+    AJS.$("#kdp-user-select-form").submit(function (e) {
+        e.preventDefault();
+        AJS.$(AJS.$('#kdp-user-select').select2("data")).each(function () {
+            showSuccessFlag(this.id);
+        });
+    });
+};
+
+AJS.toInit(function(){
+    AJS.log('KDP: Planning Page Controller initializing ...');
+    var baseUrl = AJS.params.baseURL;
+    var restUrl = baseUrl + '/rest/kitchenduty/1.0';
+
+    initUserSearch(restUrl);
 });

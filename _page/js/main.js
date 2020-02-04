14f7952dcd87858577be2f4ed74a87fa6e463416
Bernhard Grünewaldt <bernhard.gruenewaldt@comsysto.com>
2020-02-04 17:20:24 +0100

    Update main.js

--- _page/js/main.js
+++ _page/js/main.js
@@ -280,10 +280,7 @@ var _startsWith = function (stringS, prefix) {
 };
 
 var initStarsOnGitHub = function() {
-    /* dummy token from user csgruenebe with no scopes, means no rights on anything. but otherwise some users might not see the stars, because of API Rate limit */
-    $.getJSON('https://api.github.com/repos/comsysto/kitchen-duty-plugin-for-atlassian-jira/stargazers?access_token=2605ab0ec4b7c4de2a19ef458b9c24cd33876ca4', function(data) {
-        $('.cs-stars-on-github-count-stars').html(data.length);
-    });
+    /* do nothing anymore */
 };
 
 /* ================================================================================================ */
